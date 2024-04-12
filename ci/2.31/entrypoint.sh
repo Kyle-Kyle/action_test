33478fc0d7e41ad0ae0a6d2142bdc361ebd4d7b1
Kyle Zeng <zengyhkyle@gmail.com>
2024-04-11 22:52:55 -0700

    duh

---
+++ ci/2.31/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+REF=${GITHUB_REF##*/}
+git clone https://github.com/shellphish/how2heap /how2heap
+
+cd /how2heap
+
+git fetch origin $REF:ci_test
+git checkout ci_test
+make test target=2.31
