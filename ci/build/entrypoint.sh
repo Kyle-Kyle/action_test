8be39f5dbcbfea3c1a760f33d0613549b5175487
Kyle Zeng <zengyhkyle@gmail.com>
2024-04-12 13:59:16 -0700

    a

--- ci/build/entrypoint.sh
+++ ci/build/entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-echo ubuntu-version
-echo $1
-
-REF=${GITHUB_REF##*/}
 git clone https://github.com/shellphish/how2heap /how2heap
-
 cd /how2heap
-
-git fetch origin $REF:ci_test
-git checkout ci_test
 make
